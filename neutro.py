c0a0c41ba2d689d67b7eb2e1fe0b312baa71fce3
Sam <87244866+srmck@users.noreply.github.com>
2024-04-10 11:18:34 -0400

    Started CG calculator

--- neutro.py
+++
@@ -1,8 +0,0 @@
-# Collection of useful constants, functions, and procedures that are commonly used across all of my calculations in neutron scattering 
-# calcuations. When run, will perform various tests of the functions defined here.
-
-import numpy as np
-import sympy as sym
-import scipy as sc
-
-
